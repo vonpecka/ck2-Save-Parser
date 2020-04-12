1b5b0463407012d73ecc324be1596a2f2c95decf
peksa <mikulas.peksa@centrum.cz>
2020-04-12 17:08:12 +0200

    refactoring class Parser #3

--- parser.cpp
+++ parser.cpp
@@ -1,2 +1,64 @@
 #include "parser.h"
 
+namespace ck2
+{
+
+// Returns Pair of strings given a string of a property
+StringPair Parser::getProperty(std::string line, char del) const
+{
+    StringPair r;
+
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (line[i] != del) continue;
+
+        r.first  = std::string(line.begin(), line.begin() + i);
+        r.second = std::string(line.begin() + i + 1, line.end());
+
+        break;
+    }
+
+    return r;
+}
+
+// Determines whether or not a given character is in a given string
+bool Parser::contains(std::string line, char c) const
+{
+    for (int i = 0; i < line.length(); i++)
+        if (line[i] == c)
+            return true;
+
+    return false;
+}
+
+// Replaces all occurences of one character with another
+std::string Parser::replace(std::string line, char c, char rep) const
+{
+    std::string r;
+
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (line[i] != c)
+            r += line[i];
+        else
+            r += rep;
+    }
+
+    return r;
+}
+
+// Removes all occurrences of a character
+std::string Parser::remove(std::string line, char c) const
+{
+    std::string r;
+
+    for (int i = 0; i < line.length(); i++)
+    {
+        if (line[i] != c)
+            r += line[i];
+    }
+
+    return r;
+}
+
+}
--- parser.h
+++ parser.h
@@ -14,62 +14,16 @@ namespace ck2
     protected:
 
         // Returns Pair of strings given a string of a property
-        StringPair getProperty(std::string line, char del = '=') const
-        {
-            StringPair r;
-
-            for (int i = 0; i < line.length(); i++)
-            {
-                if (line[i] != del) continue;
-
-                r.first  = std::string(line.begin(), line.begin() + i);
-                r.second = std::string(line.begin() + i + 1, line.end());
-
-                break;
-            }
-
-            return r;
-        }
+        StringPair getProperty(std::string line, char del = '=') const;
 
         // Determines whether or not a given character is in a given string
-        bool contains(std::string line, char c) const
-        {
-            for (int i = 0; i < line.length(); i++)
-                if (line[i] == c)
-                    return true;
-
-            return false;
-        }
+        bool contains(std::string line, char c) const;
 
         // Replaces all occurences of one character with another
-        std::string replace(std::string line, char c, char rep) const
-        {
-            std::string r;
-
-            for (int i = 0; i < line.length(); i++)
-            {
-                if (line[i] != c)
-                    r += line[i];
-                else
-                    r += rep;
-            }
-
-            return r;
-        }
+        std::string replace(std::string line, char c, char rep) const;
 
         // Removes all occurrences of a character
-        std::string remove(std::string line, char c) const
-        {
-            std::string r;
-
-            for (int i = 0; i < line.length(); i++)
-            {
-                if (line[i] != c)
-                    r += line[i];
-            }
-
-            return r;
-        }
+        std::string remove(std::string line, char c) const;
     };
 }
 
